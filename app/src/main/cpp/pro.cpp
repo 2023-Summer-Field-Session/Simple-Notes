fa80b7da69554d7b340ef93ffde2e12e37805d3b
alex-gude <hwgude@mines.edu>
2023-05-25 17:09:24 -0600

    fixed a typo that led to a crash

--- app/src/main/cpp/pro.cpp
+++ app/src/main/cpp/pro.cpp
@@ -5,10 +5,9 @@
 
 extern "C" {
 JNIEXPORT jstring /* JNICALL  this code for whatever reason isn't needed but it complains if I don't have it */
-Java_com_simplemobiletools_notes_pro_activities_MainActivity_00024Companion_getHelloWorldString(
-        JNIEnv *env, jobject thiz) {
-jstring result = env->NewStringUTF("CPP CALL RESULT");
-__android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "CPP FILE LOG TEST");
-return result;
-}
+    Java_com_simplemobiletools_notes_pro_activities_MainActivity_00024Companion_getHelloWorldString(JNIEnv *env, jobject thiz) {
+    jstring result = env->NewStringUTF("CPP CALL RESULT");
+    __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "CPP FILE LOG TEST");
+    return result;
+    }
 }
